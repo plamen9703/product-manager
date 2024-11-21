1e928b457cbc103d7d46e71201b829fe24b20d00
plamen.hristov <plamen.hristov@LAB28-T.school.npmg.org>
2024-11-21 10:31:54 +0200

    product set and get methods and constructors

--- ProductManager/src/store/inventory/Product.java
+++ ProductManager/src/store/inventory/Product.java
@@ -3,4 +3,38 @@ public class Product {
 	private String name;
 	private double price;
 	private int amount;
+	
+	public Product() {
+		this("chips", 2.5, 10);
+	}
+	public Product(String name, double price, int amount) {
+		setName(name);
+		setPrice(price);
+		setAmount(amount);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setName(String name) {
+		if(name!=null && name.isEmpty()==false)
+			this.name=name;
+	}
+	
+	public void setPrice(double price) {
+		if(price>0)
+			this.price=price;
+	}
+	public void setAmount(int amount) {
+		if(amount>0) {
+			this.amount=amount;
+		}
+	}
 }
